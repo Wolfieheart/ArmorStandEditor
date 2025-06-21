d4272a5b796452235a6a86c12d5c24a30e3df9cd
Wolfieheart <xmakeitrain94@gmail.com>
2025-06-21 10:24:03 +0200

    [CORE] Version bump to 1.21.6-48.4
    Signed-off-by: Wolfieheart <xmakeitrain94@gmail.com>

--- src/main/java/io/github/rypofalem/armorstandeditor/ArmorStandEditorPlugin.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/ArmorStandEditorPlugin.java
@@ -60,7 +60,7 @@ public class ArmorStandEditorPlugin extends JavaPlugin {
     String nmsVersionNotLatest = null;
 
     //Hardcode the ASE Version
-    public static final String ASE_VERSION = "1.21.5-48.3";
+    public static final String ASE_VERSION = "1.21.6-48.4_rc1";
     public static final String SEPARATOR_FIELD = "================================";
 
     public PlayerEditorManager editorManager;
