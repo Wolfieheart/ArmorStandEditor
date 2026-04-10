b4f7cad1eb8711e41119f2ddbbbee086a4385a2d
Wolfieheart <xmakeitrain94@gmail.com>
2026-04-10 20:31:09 +0200

    [CORE] Make the check a bit more in line with how it is for Towny or the other Protections. Looks good otherwise!

--- src/main/java/io/github/rypofalem/armorstandeditor/protections/BoltProtection.java
+++ src/main/java/io/github/rypofalem/armorstandeditor/protections/BoltProtection.java
@@ -1,5 +1,7 @@
 package io.github.rypofalem.armorstandeditor.protections;
 
+import io.github.rypofalem.armorstandeditor.ArmorStandEditorPlugin;
+import io.github.rypofalem.armorstandeditor.Debug;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -11,19 +13,26 @@ public class BoltProtection implements Protection {
     // https://modrinth.com/plugin/bolt
 
     private final boolean boltenabled;
-    private final BoltAPI bolt = Bukkit.getServer().getServicesManager().load(BoltAPI.class);
+    private Debug debug;
+    private ArmorStandEditorPlugin plugin;
 
-    public BoltProtection() {boltenabled = Bukkit.getPluginManager().isPluginEnabled("Bolt");}
+    public BoltProtection() {
+        boltenabled = Bukkit.getPluginManager().isPluginEnabled("Bolt");
+        this.debug = new Debug(plugin);
+        debug.log("Initialized Bolt Protection for ASE. Bolt Protection is " + (boltenabled ? "enabled." : "disabled."));
+        this.plugin = ArmorStandEditorPlugin.instance();
+    }
 
     public boolean checkPermission(Entity entity, Player player) {
+        if (!boltenabled || player.isOp() || player.hasPermission("asedit.ignoreProtection.bolt")) return true;
+        debug.log("Checking Bolt Protection for player " + player.getName() + " on entity " + entity.getUniqueId());
 
-        if (!boltenabled) return true;
-        if (player.isOp() || player.hasPermission("asedit.ignoreProtection.bolt")) return true;
+        //Failsafe: Only use the API if the plugin is enabled, otherwise it will throw an error
+        BoltAPI boltAPI = Bukkit.getServer().getServicesManager().load(BoltAPI.class);
+        if(boltAPI == null) return true;
+        if (!boltAPI.isProtected(entity)) return true;
 
-        if (!bolt.isProtected(entity)) return true;
-
-        return bolt.canAccess(entity, player, "interact");
-
+        return boltAPI.canAccess(entity, player, "interact");
      }
 
 }
